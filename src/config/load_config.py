d30ffef22cef11cf5f2dff5cac370830667ebdd9
eatrabyo <itthisak.prk@gmail.com>
2025-08-23 21:06:29 +0700

    feat: add llm config

--- src/config/load_config.py
+++ src/config/load_config.py
@@ -9,20 +9,23 @@ load_dotenv(find_dotenv())
 
 class AppConfig:
     def __init__(self):
-        self.ollama_url = os.getenv("OLLAMA_API")
-        self.vector_db_url = os.getenv("QDRANT_URL", "http://localhost:6333")
         self.collection = os.getenv("COLLECTION_NAME")
         self.embedded_model = os.getenv("EMBEDDED_MODEL")
+        self.file_path = os.getenv("FILE_PATH")
+        self.llm_model = os.getenv("LLM_MODEL")
+        self.ollama_url = os.getenv("OLLAMA_API")
         self.postgres_host = os.getenv("POSTGRES_HOST")
-        self.postgres_port = os.getenv("POSTGRES_port")
+        self.postgres_port = os.getenv("POSTGRES_PORT")
         self.postgres_db = os.getenv("POSTGRES_DB")
         self.postgres_user = os.getenv("POSTGRES_USER")
         self.postgres_pass = os.getenv("POSTGRES_PASSWORD")
+        self.vector_db_url = os.getenv("QDRANT_URL", "http://localhost:6333")
 
     def indexer_config(self):
         config = {
             "vector_db": self.vector_db_url,
             "collection_name": self.collection,
+            "file_path": self.file_path,
         }
         return config
 
@@ -40,6 +43,13 @@ class AppConfig:
             "user": self.postgres_user,
             "pass": self.postgres_pass,
             "db": self.postgres_db,
+        }
+        return config
+
+    def llm_config(self):
+        config = {
+            "llm_model": self.llm_model,
+            "ollama_api": self.ollama_url,
         }
         return config
 
