ae32280d2c00bf167ef2c4934bc18230f983c765
eatrabyo <itthisak.prk@gmail.com>
2025-08-23 21:07:16 +0700

    feat: llm function

---
+++ src/core/llm_model.py
@@ -0,0 +1,17 @@
+from llama_index.llms.ollama import Ollama
+
+from src.config.load_config import load_config
+
+config = load_config()
+
+
+def llm_model(llm_config=config.llm_config()):
+
+    llm = Ollama(
+        model=llm_config["llm_model"],
+        temperature=0.2,
+        base_url=llm_config["ollama_api"],
+        request_timeout=600,
+        stream=True,
+    )
+    return llm
