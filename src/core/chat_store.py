146b328a5b128bf12509253aeac298641d9ed2d8
eatrabyo <itthisak.prk@gmail.com>
2025-09-13 17:22:59 +0700

    feat: implement database connection setup in chat_mem function

--- src/core/chat_store.py
+++ src/core/chat_store.py
@@ -4,16 +4,29 @@ from llama_index.storage.chat_store.postgres import PostgresChatStore
 from src.config.load_config import load_config
 
 config = load_config().postgres_config()
+from sqlalchemy import URL, create_engine
+from sqlalchemy_utils import create_database, database_exists
 
 
 def chat_mem(user_id):
+    url_object = URL.create(
+        "postgresql+psycopg2",
+        username=config["user"],
+        password=config["pass"],  # plain (unescaped) text
+        host=config["host"],
+        port=config["port"],
+        database=config["db"],
+    )
+    engine = create_engine(url_object)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
     chat_store = PostgresChatStore.from_params(
         host=config["host"],
         port=config["port"],
         user=config["user"],
         password=config["pass"],
         database=config["db"],
-        table_name="chat_history",
     )
 
     chat_memory = ChatMemoryBuffer.from_defaults(
